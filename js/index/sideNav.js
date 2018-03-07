f0bd1b0c7dcdb3bc6e556ecd296b384bf5193201
Capuccino <chinodesuuu@gmail.com>
2018-03-07 00:32:46 +0000

    point FAB to new shit

--- js/index/sideNav.js
+++ js/index/sideNav.js
@@ -1,5 +1,10 @@
 var elem = document.querySelector('.sidenav');
+var trigger = document.getElementById('hamburger');
 var instance = M.Sidenav.init(elem, {
   edge: 'left',
   preventScrolling: true
 });
+
+document.addEventListener('click', function () {
+  instance.open();
+},trigger);
