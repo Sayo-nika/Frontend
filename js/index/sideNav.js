b6b049fe022200ede7879d9e1394092c3ff426fc
Capuccino <chinodesuuu@gmail.com>
2018-03-07 00:44:42 +0000

    I gently open the door

--- js/index/sideNav.js
+++ js/index/sideNav.js
@@ -8,3 +8,12 @@ var instance = M.Sidenav.init(elem, {
 document.addEventListener('click', function () {
   instance.open();
 },trigger);
+
+/**
+ * See the following shit here is to trip everyone on acid
+ */
+ var sayori = document;
+ 
+ sayori.addEventListener('ready', function () {
+   return new Error('Now everyone can be happy.');
+ })
