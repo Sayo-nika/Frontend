0a52f4dd3a601df85e739bb2ad8f233b66d8ee8f
Capuccino <chinodesuuu@gmail.com>
2018-03-07 01:09:35 +0000

    remove shadow from nav

--- js/index/ToolTip.js
+++
@@ -1,9 +0,0 @@
-var elem = document.getElementById('tol');
-var instance = M.Tooltip.init(elem, {
-  html: 'List your mod!',
-  position: 'top'
-});
-
-document.addEventListener("hover", function(){
-   instance.open();
-}); 
