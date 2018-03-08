9f07c6b361b1f533e7efed8d499156a84fe72be5
Ovyerus <iamovyerus@gmail.com>
2018-03-08 18:23:38 +1100

    Add carousels, donate dropdown, copyright text, and clean up CSS

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 M.Tooltip.init(document.querySelector('.tooltipped'));
 M.Sidenav.init(document.querySelector('.sidenav'));
 M.Modal.init(document.querySelector('.modal'));
+M.Dropdown.init(document.querySelector('.dropdown-trigger'));
+document.querySelectorAll('.carousel').forEach(el => M.Carousel.init(el, {indicators: true, dist: 0}));
