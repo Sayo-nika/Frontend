5eea91bf3fb172eb17fca6f544838f24b783861d
Ovyerus <iamovyerus@gmail.com>
2018-04-17 20:52:58 +1000

    Attribute arrangment adjustment in FAB

--- src/components/FAB/index.js
+++ src/components/FAB/index.js
@@ -5,22 +5,19 @@ import './FAB.sass';
 
 export const FAB = ({href, label, icon, mini, oncreate=() => {}, ondestroy=() => {}, noripple, class: class_, ...rest}) => {
     let classes = 'mdc-fab material-icons' + (mini ? ' mdc-fab--mini' : '') + (class_ ? ` ${class_}` : '');
-    let onCreate = noripple ? oncreate : el => {
-        el.ripple = new MDCRipple(el);
-        oncreate(el);
-    };
-    let onDestroy = noripple ? ondestroy : el => {
-        el.ripple.destroy();
-        ondestroy(el);
-    };
-
     let inner = <span class="mdc-fab__icon">{icon}</span>;
     let attrs = {
         class: classes,
         href,
         'aria-label': label,
-        oncreate: onCreate,
-        ondestroy: onDestroy,
+        oncreate: noripple ? oncreate : el => {
+            el.ripple = MDCRipple.attachTo(el);
+            oncreate(el);
+        },
+        ondestroy: noripple ? ondestroy : el => {
+            el.ripple.destroy(el);
+            ondestroy(el);
+        },
         ...rest
     };
 
