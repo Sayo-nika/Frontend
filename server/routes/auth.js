c2afffc1652d4f8bae5169d0a736a2fe3e415edf
Capuccino <chinodesuuu@gmail.com>
2018-06-04 14:32:29 +0000

    add basic server for now

---
+++ server/routes/auth.js
@@ -0,0 +1,11 @@
+/**
+ * @file auth.js
+ * @description Authorization broker endpoint - Passport.js-based.
+ * @author Capuccino
+ */
+
+/* global router */
+
+router.get('/api/auth', () => {
+    // leave this be for now 
+})
