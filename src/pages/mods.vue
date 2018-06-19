21ca2dcfdc74584f5333e9b2bdf1e1e25a4efbe6
Ovyerus <iamovyerus@gmail.com>
2018-06-19 14:29:56 +1000

    Add basic mod search view

--- src/pages/mods.vue
+++ src/pages/mods.vue
@@ -1,48 +1,8 @@
 <template>
   <div>
-    <!-- <div class="md-layout md-gutter">
-      <div class="md-layout-item md-xlarge-size-33 md-large-size-33 md-medium-size-50 md-small-size-50 md-xsmall-size-100">
-        <md-card md-with-hover>
-          <md-card-area>
-            <md-ripple>
-              <a class="no-deco" @click.prevent="cardModal = true">
-                <md-card-media md-ratio="16:9">
-                  <img src="https://material.io/design/assets/1kVVLIES2HDnnmqXgAvglbAK8a-oVEEh0/intro-illo-motion.png" alt="Placeholder">
-                </md-card-media>
-
-                <md-card-header>
-                  <div class="md-title">Foo</div>
-                  <div class="md-subhead">by Bar</div>
-                </md-card-header>
-              </a>
-            </md-ripple>
-          </md-card-area>
-
-          <md-card-expand>
-            <md-card-actions md-alignment="space-between">
-              <div>
-                <md-button class="md-accent">Download</md-button>
-              </div>
-
-              <md-card-expand-trigger>
-                <md-button>More</md-button>
-              </md-card-expand-trigger>
-            </md-card-actions>
-
-            <md-card-expand-content>
-              <md-card-content>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                In faucibus pellentesque consectetur. Aenean pretium tortor eros, vitae iaculis orci vehicula eget.
-                Nunc commodo lorem id est suscipit iaculis.
-                Ut finibus nisi et pellentesque porttitor.
-              </md-card-content>
-            </md-card-expand-content>
-          </md-card-expand>
-        </md-card>
-      </div>
-    </div>
-
-    <md-dialog-alert :md-active.sync="cardModal" md-content="Haha u pressed the card butan"/> -->
+    <v-text-field id="search-box" v-model="search" :class="{'elevation-6': searchInFocus}"
+                  label="Search" prepend-icon="search" solo
+                  @blur="searchInFocus = false" @focus="searchInFocus = true"/>
   </div>
 </template>
 
@@ -51,7 +11,15 @@ export default {
   layout: 'mods',
   data() {
     return {
-      cardModal: false
+      search: '',
+      searchInFocus: false,
+      categoryFilter: null,
+      categories: {
+        released: 'Released',
+        playtesting: 'Playtesting',
+        inDevelopment: 'In Development',
+        planning: 'Planning'
+      }
     };
   }
 };
