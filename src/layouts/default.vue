156d27a47a6374538a57c210ec009a37c7b43f17
Ovyerus <iamovyerus@gmail.com>
2018-06-20 18:21:50 +1000

    Add footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -35,17 +35,26 @@
       </div>
     </v-content>
 
-    <!-- <v-footer height="auto" class="primary darken-1" app absolute>
-      <v-layout row wrap justify-center>
-        <v-btn color="white" flat>Home</v-btn>
-        <v-btn color="white" flat>Mod List</v-btn>
-        <v-btn color="white" flat>About Us</v-btn>
+    <v-footer class="py-3 px-3" color="grey darken-3" height="auto" absolute app>
+      <v-container>
+        <v-layout row wrap justify-center>
+          <v-flex class="text-xs-center mb-3" xs12 white--text>
+            Doki Doki Literature Club (DDLC), logo, and artworks are properties of Team Salvato, All Rights Reserved.<br>
+            Sayonika, the Sayonika chibi artwork, and the Sayonika logo are licensed under Creative Commons-3.0 Non-Commercial.<br>
+            Sayonika's source code is released under the BSD-3 Clause License.<br>
+            All Doki Doki Literature Club mods listed on this site complies to the Team Salvato guidelines stated therein.
+          </v-flex>
 
-        <v-flex xs12 py-3 text-xs-center white--text>
-          &copy;2018 - <b>Sayonika</b>
-        </v-flex>
-      </v-layout>
-    </v-footer> -->
+          <v-btn color="white" to="/dmca" nuxt outline round>DMCA</v-btn>
+          <v-btn color="white" href="https://github.com/Sayo-nika" outline round>Open Source</v-btn>
+          <v-btn color="white" to="/about" nuxt outline round>About</v-btn>
+
+          <v-flex xs12 mt-3 text-xs-center white--text>
+            &copy; 2018 &mdash; <b>Sayonika</b>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-footer>
   </v-app>
 </template>
 
