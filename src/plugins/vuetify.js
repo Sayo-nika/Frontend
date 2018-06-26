f21b456423ad1d31563cd194353ad1eb9c0c828a
Ovyerus <iamovyerus@gmail.com>
2018-06-26 19:28:29 +1000

    Update colours to match new ones

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import colors from 'vuetify/es5/util/colors';
 
 Vue.use(Vuetify, {
   theme: {
-    primary: colors.deepOrange.base,
-    secondary: colors.deepOrange.darken2,
-    accent: colors.orange.accent2
+    primary: '#e84444',
+    secondary: '#e73232',
+    accent: '#f13131'
   }
 });
