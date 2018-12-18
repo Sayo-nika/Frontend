f324202d0cdafeeac36fa2787febe84c250f1a30
Ovyerus <iamovyerus@gmail.com>
2018-12-18 13:44:36 +1100

    feat: temp error page

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>worlds ending i guess</h2>
+    <h1><pre><code>{{ stringError }}</code></pre></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  props: ['error'], // eslint-disable-line vue/require-prop-types
+  data() {
+    return {
+      stringError: JSON.stringify(this.error)
+    };
+  },
+  mounted() {
+    console.error(this.error);
+  }
+};
+</script>
+
+<style>
+</style>
