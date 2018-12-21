3879d64b849faa7f053901f0cdb1e06106f571ae
Capuccino <enra@sayonika.moe>
2018-12-21 10:37:11 +0800

    feat: admin page from search layout
    Signed-off-by: Capuccino <enra@sayonika.moe>

---
+++ layouts/admin.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app clipped>
+      <v-list>
+        <v-list-tile to="/" nuxt ripple>
+          <v-list-tile-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile to="/admin/pending-mods" nuxt ripple>
+          <v-list-tile-action>
+            <v-icon>mdi-library-books</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>Pending Mods</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile to="/admin/user-reports" nuxt ripple>
+          <v-list-tile-action>
+            <v-icon>mdi-email-open</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>Flags and Reports</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+
+    <navbar no-search drawer-btn/>
+
+    <v-content>
+      <v-container fluid>
+        <nuxt/>
+      </v-container>
+    </v-content>
+
+    <!-- <v-footer app/> -->
+  </v-app>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue';
+
+export default {
+  components: {Navbar}
+};
+</script>
+
+<style>
+body {
+  background: #FAFAFA;
+}
+</style>
---
+++ pages/admin.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-layout>
+    <v-list class="search__results elevation-3" two-line>
+      <v-subheader class="primary--text">
+        Pending Mods
+      </v-subheader>
+
+      <v-list-tile v-for="i in 5" :key="`user-${i}`" avatar @click="void 0">
+        <v-list-tile-avatar>
+          <img src="https://placeimg.com/128/128/people" alt="icon">
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>Title</v-list-tile-title>
+          <v-list-tile-sub-title>*screams*</v-list-tile-sub-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+          <v-btn color="primary" depressed ripple>View</v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+
+      <v-layout class="mt-2" justify-center>
+        <v-pagination v-model="userPage" :length="10" :total-visible="paginationVisible"/>
+      </v-layout>
+    </v-list>
+    <v-list class="search__results elevation-3" two-line>
+      <v-subheader class="primary--text">
+        Recent User Reports
+      </v-subheader>
+
+      <v-list-tile v-for="i in 5" :key="`user-${i}`" avatar @click="void 0">
+        <v-list-tile-avatar>
+          <img src="https://placeimg.com/128/128/people" alt="icon">
+        </v-list-tile-avatar>
+
+        <v-list-tile-content>
+          <v-list-tile-title>Title</v-list-tile-title>
+          <v-list-tile-sub-title>*screams*</v-list-tile-sub-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+          <v-btn color="primary" depressed ripple>View</v-btn>
+        </v-list-tile-action>
+      </v-list-tile>
+
+      <v-layout class="mt-2" justify-center>
+        <v-pagination v-model="userPage" :length="10" :total-visible="paginationVisible"/>
+      </v-layout>
+    </v-list>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: 'admin'
+};
+</script>
