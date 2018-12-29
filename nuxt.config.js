995f705e1c82e1d8c57f5f6389065c6849854dc7
Capuccino <enra@sayonika.moe>
2018-12-29 23:43:21 +0800

    fix(nuxt): use custom values
    Signed-off-by: Capuccino <enra@sayonika.moe>

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,11 @@ export default {
   mode: 'universal',
 
   head: {
-    title: pkg.name,
+    title: 'Sayonika',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: pkg.description}
+      {hid: 'description', name: 'description', content: 'The official DDMC Mod Catalog.'}
     ],
     link: [
       {rel: 'icon', type: 'image/png', href: '/favicon.png'},
