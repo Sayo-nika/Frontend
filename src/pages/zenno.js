9ea6a7527f9b75946307d99647374b871dbf2c7f
Marquis Kurt <software@marquiskurt.net>
2019-08-08 17:58:45 -0400

    [src/pages] Update docs on Demo Page
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- src/pages/zenno.js
+++ src/pages/zenno.js
@@ -4,6 +4,11 @@ import Navbar from '../components/navbar';
 /**
  * This page is used to _test_ components for use in Sayonika.
  * Please do **NOT** use this page in production!
+ *
+ * If you do use this page in production, I will personally
+ * smack you silly.
+ *
+ * K thx - @alicerunsonfedora
  */
 function DemoPage() {
     return (
