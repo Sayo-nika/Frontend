52730907d3324c789970b67799083388446da5e6
Martmists <martmists@gmail.com>
2019-08-09 21:44:57 +0200

    Add footer const
    Signed-off-by: Martmists <martmists@gmail.com>

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FOOTER = <div>
+    Copyright 2019 (c) Sayonika Project Authors, Sayonika is a project by Clarity and friends.
+
+    Doki Doki Literature Club, the Logo, and artworks are Copyright Team Salvato LLC. All Rights Reserved.
+</div>;
+
+export default FOOTER;
