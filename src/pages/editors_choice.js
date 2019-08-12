cb0c178e2b8ebfa814e870871fc5ec6b4c87b1eb
Martmists <martmists@gmail.com>
2019-08-12 20:45:51 +0200

    Editor's choice page pt 3
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/pages/editors_choice.js
+++ src/pages/editors_choice.js
@@ -17,11 +17,15 @@ class EditorsChoice extends Component {
     }
 
     render() {
-        API.getMods("").then(r=>{
-            this.setState({mods:[
-                <ModCard id={0} title={"Coming Soon"} status={2} category={3}/>
-            ]})
-        });
+        if (!this.state.mods) {
+            API.getMods("").then(r => {
+                this.setState({
+                    mods: [
+                        <ModCard id={0} title={"Coming Soon"} status={2} category={3}/>
+                    ]
+                })
+            });
+        }
 
         return <div>
             <Navbar />
