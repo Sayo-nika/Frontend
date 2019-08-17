6a215264a4cd51ab267f8e7f62e5115896699376
Marquis Kurt <software@marquiskurt.net>
2019-08-17 12:07:53 -0400

    [src/pages] Convert editor's choice to function

--- src/pages/editors_choice.js
+++ src/pages/editors_choice.js
@@ -7,46 +7,93 @@ import API from '../utils/api';
 import ModCard from '../components/mod_card';
 import { useGlobalPageStyles } from '../utils/global_styles';
 
-class EditorsChoice extends Component {
-    constructor(props) {
-        super(props);
+const EditorsChoice = () => {
+    const [state, setState] = React.useState({
+        mods: []
+    });
 
-        this.state = {
-            mods: []
-        };
-    }
+    const globalClasses = useGlobalPageStyles();
 
-    componentDidMount() {
-        API.getEditorsChoiceMods('').then(r => {
-            this.setState({
-                mods: [
-                    <ModCard
-                        id={0}
-                        title={'Coming Soon'}
-                        status={2}
-                        category={3}
-                    />
-                ]
+    // More info on useEffect: https://reactjs.org/docs/hooks-effect.html
+    React.useEffect(() => {
+        if (state.mods.length <= 0) {
+            API.getEditorsChoiceMods('').then(response => {
+                setState({
+                    mods: [
+                        <ModCard
+                            id={0}
+                            title={'Coming Soon'}
+                            status={2}
+                            category={3}
+                        />
+                    ]
+                })
             });
-        });
-    }
+        }
+    });
 
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <CatalogBar />
+    return (
+        <div>
+            <Navbar/>
+            <CatalogBar/>
+            <div className={globalClasses.pageContent}>
                 <Grid container spacing={2}>
-                    {this.state.mods.map(e => (
-                        <Grid item lg={3} xs={12}>
-                            {e}
-                        </Grid>
-                    ))}
+                    {
+                        state.mods?
+                            state.mods.map(mod => (
+                                <Grid item xs={12} md={6} lg={3}>
+                                    {mod}
+                                </Grid>
+                            ))
+                        : <div>No editor's choice mods available.</div>
+                    }
                 </Grid>
-                <Footer />
             </div>
-        );
-    }
+            <Footer/>
+        </div>
+    )
 }
+
+// class EditorsChoice extends Component {
+//     constructor(props) {
+//         super(props);
+
+//         this.state = {
+//             mods: []
+//         };
+//     }
+
+//     componentDidMount() {
+//         API.getEditorsChoiceMods('').then(r => {
+//             this.setState({
+//                 mods: [
+//                     <ModCard
+//                         id={0}
+//                         title={'Coming Soon'}
+//                         status={2}
+//                         category={3}
+//                     />
+//                 ]
+//             });
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <Navbar />
+//                 <CatalogBar />
+//                 <Grid container spacing={2}>
+//                     {this.state.mods.map(e => (
+//                         <Grid item lg={3} xs={12}>
+//                             {e}
+//                         </Grid>
+//                     ))}
+//                 </Grid>
+//                 <Footer />
+//             </div>
+//         );
+//     }
+// }
 
 export default EditorsChoice;
