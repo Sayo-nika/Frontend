c28cf15f2d66cc5bd0bfe187bdbf90b046a6a462
Marquis Kurt <software@marquiskurt.net>
2019-08-17 12:21:59 -0400

    [src/components] Match footer more closely to spec

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(2),
+        backgroundColor: "#333333",
+        color: theme.palette.common.white,
+        textAlign: 'center'
+    }
+}))
 
 const Footer = () => {
+    const classes = useStyles();
+
     return (
-        <div style={{ padding: 16 }}>
+        <div className={classes.root}>
             <Typography variant="caption">
                 Copyright 2018-{new Date().getFullYear()} (c) Sayonika Project
                 Authors.
