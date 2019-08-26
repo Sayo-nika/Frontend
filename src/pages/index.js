b1678b289f117fe87e920a0e4baa71883fe7f7ca
Martmists <martmists@gmail.com>
2019-08-26 14:27:25 +0200

    Run Prettier
    Signed-off-by: Martmists <martmists@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,6 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const IndexPage = props => {
-
     const [state, setState] = React.useState({
         articles: [],
         new: [],
@@ -30,42 +29,48 @@ const IndexPage = props => {
     React.useEffect(() => {
         API.getMods('').then(response => {
             setState(prevState => {
-                return {...prevState,
-                new: [
-                    <ModCard
-                        id={0}
-                        title={"Obama's Sexy Time Club"}
-                        status={2}
-                        category={3}
-                    />
-                ]}
-            })
+                return {
+                    ...prevState,
+                    new: [
+                        <ModCard
+                            id={0}
+                            title={"Obama's Sexy Time Club"}
+                            status={2}
+                            category={3}
+                        />
+                    ]
+                };
+            });
         });
         API.getMods('').then(response => {
             setState(prevState => {
-                return {...prevState,
-                favorites: [
-                    <ModCard
-                        id={0}
-                        title={'Doki Doki Azzy Club'}
-                        status={2}
-                        category={3}
-                    />
-                ]}
-            })
+                return {
+                    ...prevState,
+                    favorites: [
+                        <ModCard
+                            id={0}
+                            title={'Doki Doki Azzy Club'}
+                            status={2}
+                            category={3}
+                        />
+                    ]
+                };
+            });
         });
         API.getMods('').then(response => {
             setState(prevState => {
-                return {...prevState,
-                wip: [
-                    <ModCard
-                        id={0}
-                        title={'e did it 😂🔫'}
-                        status={2}
-                        category={3}
-                    />
-                ]}
-            })
+                return {
+                    ...prevState,
+                    wip: [
+                        <ModCard
+                            id={0}
+                            title={'e did it 😂🔫'}
+                            status={2}
+                            category={3}
+                        />
+                    ]
+                };
+            });
         });
     }, []);
 
@@ -77,47 +82,55 @@ const IndexPage = props => {
             <Navbar />
             <CatalogBar />
             <div className={globalClasses.pageContent}>
-                <Typography>Carousel would go here... if we had any!</Typography>
+                <Typography>
+                    Carousel would go here... if we had any!
+                </Typography>
                 <div className={classes.strip}>
-                    <Typography variant="h5" component="h2">Fresh from the Oven</Typography>
+                    <Typography variant="h5" component="h2">
+                        Fresh from the Oven
+                    </Typography>
                     <Grid container spacing={2}>
-                        {
-                            state.new?
-                                state.new.map(mod => (
-                                    <Grid item xs={12} md={6} lg={3}>
-                                        {mod}
-                                    </Grid>
-                                )):
-                                <Typography>No new mods yet</Typography>
-                        }
+                        {state.new ? (
+                            state.new.map(mod => (
+                                <Grid item xs={12} md={6} lg={3}>
+                                    {mod}
+                                </Grid>
+                            ))
+                        ) : (
+                            <Typography>No new mods yet</Typography>
+                        )}
                     </Grid>
                 </div>
                 <div className={classes.strip}>
-                    <Typography variant="h5" component="h2">Most Loved</Typography>
+                    <Typography variant="h5" component="h2">
+                        Most Loved
+                    </Typography>
                     <Grid container spacing={2}>
-                        {
-                            state.favorites?
-                                state.favorites.map(mod => (
-                                    <Grid item xs={12} md={6} lg={3}>
-                                        {mod}
-                                    </Grid>
-                                )):
-                                <Typography>No new mods yet</Typography>
-                        }
+                        {state.favorites ? (
+                            state.favorites.map(mod => (
+                                <Grid item xs={12} md={6} lg={3}>
+                                    {mod}
+                                </Grid>
+                            ))
+                        ) : (
+                            <Typography>No new mods yet</Typography>
+                        )}
                     </Grid>
                 </div>
                 <div className={classes.strip}>
-                    <Typography variant="h5" component="h2">Coming Soon</Typography>
+                    <Typography variant="h5" component="h2">
+                        Coming Soon
+                    </Typography>
                     <Grid container spacing={2}>
-                        {
-                            state.wip?
-                                state.wip.map(mod => (
-                                    <Grid item xs={12} md={6} lg={3}>
-                                        {mod}
-                                    </Grid>
-                                )):
-                                <Typography>No new mods yet</Typography>
-                        }
+                        {state.wip ? (
+                            state.wip.map(mod => (
+                                <Grid item xs={12} md={6} lg={3}>
+                                    {mod}
+                                </Grid>
+                            ))
+                        ) : (
+                            <Typography>No new mods yet</Typography>
+                        )}
                     </Grid>
                 </div>
             </div>
