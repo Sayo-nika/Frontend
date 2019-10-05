59059c6b7130d2a56189c91177e0b86cd5df6cfa
Marquis Kurt <software@marquiskurt.net>
2019-10-05 12:30:20 -0400

    [src/components] Update footer text with Xhunzei credit
    Signed-off-by: Marquis Kurt <software@marquiskurt.net>

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,11 +5,11 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles(theme => ({
     root: {
         padding: theme.spacing(2),
-        backgroundColor: "#333333",
+        backgroundColor: '#333333',
         color: theme.palette.common.white,
         textAlign: 'center'
     }
-}))
+}));
 
 const Footer = () => {
     const classes = useStyles();
@@ -23,7 +23,8 @@ const Footer = () => {
             <Typography paragraph variant="caption">
                 Sayonika is a project by Clarity and friends. Doki Doki
                 Literature Club, the Logo, and artworks are Copyright Team
-                Salvato LLC. All Rights Reserved.
+                Salvato LLC. Login artwork created by Xhunzei. All Rights
+                Reserved.
             </Typography>
         </div>
     );
