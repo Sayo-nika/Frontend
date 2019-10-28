760b52b794dc4ccdcabbaab647ea2f72e2425674
Ovyerus <iamovyerus@gmail.com>
2019-10-28 22:13:27 +1100

    Use react-dom to render app

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-
-import App from './app';
-
-AppRegistry.registerComponent('Sayonika', () => App);
-
-AppRegistry.runApplication('Sayonika', {
-  rootTag: document.getElementById('root')
-});
