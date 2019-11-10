da116d1a40f3b4d79464134c0db0d9f03619d331
Ovyerus <iamovyerus@gmail.com>
2019-11-10 11:30:51 +1100

    [app] Add Switch to prevent dupes

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // import Navbar from './components/navbar';
 import IndexPage from './pages';
@@ -10,7 +10,7 @@ import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
 import ModPage from './pages/mod';
 import SayonikaTheme from './utils/theme';
-import UserPage from './pages/user';
+import ProfilePage from './pages/profile';
 // import EditorsChoice from './pages/editors_choice';
 // import Trending from './pages/trending';
 // import AllModsPage from './pages/all';
@@ -22,17 +22,20 @@ const App = () => (
   <Router>
     <CssBaseline />
     <ThemeProvider theme={theme}>
+      <Switch>
       <Route path="/" component={IndexPage} exact />
-      <Route path="/mods/:id" component={ModPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/signup" component={SignupPage} />
-      <Route path="/users/:id" component={UserPage} />
+        {/* TOOD: do we wanna have more reviews button change the route? */}
+        <Route path="/mods/:id" component={ModPage} exact />
+        <Route path="/login" component={LoginPage} exact />
+        <Route path="/signup" component={SignupPage} exact />
+        <Route path="/profile/:id" component={ProfilePage} exact />
       {/* <Route path="/index" component={IndexPage} />
       <Route path="/mods" component={ModList} />
       <Route path="/editors_choice" component={EditorsChoice} />
       <Route path="/trending" component={Trending} />
       <Route path="/all" component={AllModsPage} />
       <Route path="/error/:id" component={ErrorPage} /> */}
+      </Switch>
     </ThemeProvider>
   </Router>
 );
