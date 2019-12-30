5f17f59517ab39e00a38fde4d29a0b138f10599d
Ovyerus <iamovyerus@gmail.com>
2019-12-30 15:05:55 +1100

    [utils/api] Add unpack helper and headers
    Unpack helper unwraps responses from the api into just their results, to work with elsewhere easier.

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,47 +3,52 @@ import bent from 'bent';
 import config from './config';
 
 const qs = obj => new URLSearchParams(obj).toString();
+/** Higher-order-function to transform API responses into just their results */
+const unpack = request => (...args) => request(...args).then(re => re.result);
 
-// TODO : tokens
-export const get = bent(`${config.BASE_URL}/api/v1/`, 'GET', 'json');
-export const post = bent(`${config.BASE_URL}/api/v1/`, 'POST', 'json');
-export const patch = bent(`${config.BASE_URL}/api/v1/`, 'PATCH', 'json');
-export const del = bent(`${config.BASE_URL}/api/v1/`, 'DELETE', 'json');
+const apiBase = `${config.BASE_URL}/api/v1/`;
+const headers = {};
+export const setHeader = (name, value) => void (headers[name] = value);
+
+export const get = unpack(bent(apiBase, 'GET', 'json', headers));
+export const post = unpack(bent(apiBase, 'POST', 'json', headers));
+export const patch = unpack(bent(apiBase, 'PATCH', 'json', headers));
+export const del = unpack(bent(apiBase, 'DELETE', 'json', headers));
 
 export const login = (username, password, recaptcha) =>
   post('/login', { username, password, recaptcha });
 export const signup = (username, email, password, recaptcha) =>
   post('/users', { username, email, password, recaptcha });
 
-export const getMod = id => get(`/mods/${id}`);
-export const getMods = (options = {}) => get('/mods', options);
-export const getTrendingMods = () => get('/mods/trending');
-export const getEditorsChoice = () => get('/mods/editors_choice');
-export const getRecentMods = () => get('/mods/recent_releases');
-export const getMostLovedMods = () => get('/mods/most_loved');
-export const getMostDownloadedMods = () => get('/mods/most_downloads');
-export const getModReviews = id => get(`/mods/${id}/reviews`);
-export const getModAuthors = id => get(`/mods/${id}/authors`);
+export const getMod = id => get(`mods/${id}`);
+export const getMods = (options = {}) => get(`mods?${qs(options)}`);
+export const getTrendingMods = () => get('mods/trending');
+export const getEditorsChoice = () => get('mods/editors_choice');
+export const getRecentMods = () => get('mods/recent_releases');
+export const getMostLovedMods = () => get('mods/most_loved');
+export const getMostDownloadedMods = () => get('mods/most_downloads');
+export const getModReviews = id => get(`mods/${id}/reviews`);
+export const getModAuthors = id => get(`mods/${id}/authors`);
 
-export const getUser = id => get(`/users/${id}`);
-export const getUsers = (options = {}) => get('/users', options);
-export const getUserFavorites = id => get(`/users/${id}/favorites`);
-export const getUserMods = id => get(`/users/${id}/mods`);
-export const getUserReviews = id => get(`/users/${id}/reviews`);
+export const getUser = id => get(`users/${id}`);
+export const getUsers = (options = {}) => get('users', options);
+export const getUserFavorites = id => get(`users/${id}/favorites`);
+export const getUserMods = id => get(`users/${id}/mods`);
+export const getUserReviews = id => get(`users/${id}/reviews`);
 
-export const createMod = options => post('/mods', options);
+export const createMod = options => post('mods', options);
 export const createModReview = (id, title, rating, content) =>
-  post(`/mods/${id}/reviews`, { title, rating, content });
-export const patchSelf = (options = {}) => patch('/users/@me', options);
-export const patchMod = (id, options = {}) => patch(`/mods/${id}`, options);
-export const deleteMod = id => del(`/mods/${id}`);
+  post(`mods/${id}/reviews`, { title, rating, content });
+export const patchSelf = (options = {}) => patch('users/@me', options);
+export const patchMod = (id, options = {}) => patch(`mods/${id}`, options);
+export const deleteMod = id => del(`mods/${id}`);
 
 export const reportUser = (id, content, type_, recaptcha) =>
-  post(`/users/${id}/report`, { content, type_, recaptcha });
+  post(`users/${id}/report`, { content, type_, recaptcha });
 export const reportMod = (id, content, type_, recaptcha) =>
-  post(`/mods/${id}/report`, { content, type_, recaptcha });
+  post(`mods/${id}/report`, { content, type_, recaptcha });
 
-export const getNews = () => get('/news');
+export const getNews = () => get('news');
 export const getFrontpage = () =>
   Promise.all([
     getNews(),
@@ -52,4 +57,4 @@ export const getFrontpage = () =>
     getTrendingMods(),
     getMods({ limit: 5, status: 'in_development' })
   ]);
-export const search = q => get(`/search?${qs({ q })}`);
+export const search = q => get(`search?${qs({ q })}`);
