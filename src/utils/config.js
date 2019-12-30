dbd28798ce08cdb68c3fa309c5d910a2596b521c
Ovyerus <iamovyerus@gmail.com>
2019-12-30 15:04:11 +1100

    [utils/config] Take config from environment

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,7 @@
 const config = {
-  BASE_URL: 'https://stg-backend.sayonika.moe',
-  CAPTCHA_KEY: '6LcxIZEUAAAAAN903uVIt6pL6OcduwKKPJ9XArnj'
+  BASE_URL:
+    process.env.REACT_APP_API_BASE_URL || 'https://stg-backend.sayonika.moe',
+  CAPTCHA_KEY: process.env.REACT_APP_RECAPTCHA_SITE_KEY
 };
 
 export default config;
